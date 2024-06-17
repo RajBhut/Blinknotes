8a21752be90b08afbf29193f2a351232c659184e
Raj Bhut <rajbhut2832005@gmail.com>
2024-06-17 12:47:12 +0530

    redy for deploy

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ function App() {
     
     try{const response = await fetch(`https://blinknotess-f1199a4df86d.herokuapp.com/api/notes/k/${searchvalue}` , {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },mode: 'no-cors',
+     mode: 'no-cors',
     
     });  
 
